9fb188db49f89179e3bebe8001eda697697e715d
amanattar <attar.aman29@gmail.com>
2024-10-23 01:07:54 -0500

    version upgraded

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rufus_crawler",  # Name of your package on PyPI
-    version="0.1.0",
+    version="0.1.1",
     description="A web crawler and content filtering tool using OpenAI.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
